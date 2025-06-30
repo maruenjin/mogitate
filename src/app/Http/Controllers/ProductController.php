71ae791664654ec049704e8f6b2e287487297f58
maruyu <goldenleaf3310@gmail.com>
2025-06-30 23:30:55 +0900

    コントローラ作成

--- src/app/Http/Controllers/ProductController.php
+++ src/app/Http/Controllers/ProductController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function create()
+    {
+        return view('products.create');
+    }
+    
+    public function index()
+{
+    return view('products.index');
+}
+
 }
