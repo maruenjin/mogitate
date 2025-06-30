d82eafc5ed68b0654eb84f5a41c96340a1526c0f
maruyu <goldenleaf3310@gmail.com>
2025-07-01 00:27:24 +0900

    app.blade.php修正

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -3,8 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>mogitate</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
+    <header>
+        <h1>mogitate</h1>
+    </header>
+    
     @yield('content')
 </body>
 </html>
