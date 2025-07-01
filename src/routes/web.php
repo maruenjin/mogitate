84db34dd506626f22c5d42c5b046f59308b1dbc0
maruyu <goldenleaf3310@gmail.com>
2025-07-01 09:47:33 +0900

    storeルーティング追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -20,3 +20,4 @@ Route::get('/', function () {
 Route::get('/products/register', [ProductController::class, 'create'])->name('products.create');
 Route::post('/products/register', [ProductController::class, 'store'])->name('products.store');
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');
