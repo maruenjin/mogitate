409f4860fcfd47a71a24184c73b980271674378b
maruyu <goldenleaf3310@gmail.com>
2025-07-02 23:05:25 +0900

    ルーティング修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 Route::get('/products/register', [ProductController::class, 'create'])->name('products.create');
 Route::post('/products/register', [ProductController::class, 'store'])->name('products.store');
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/products/{product}', [ProductController::class, 'show'])
-    ->name('products.show');
+Route::get('/products/search', [ProductController::class, 'search'])->name('products.search');
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+
+
