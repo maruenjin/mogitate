26c1dac69423bccf37e5cea5a0706ca48e558683
maruyu <goldenleaf3310@gmail.com>
2025-07-03 11:26:19 +0900

    ルーティング追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -22,5 +22,8 @@ Route::post('/products/register', [ProductController::class, 'store'])->name('pr
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 Route::get('/products/search', [ProductController::class, 'search'])->name('products.search');
 Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
+Route::get('/products/{product}/update', [ProductController::class, 'edit'])->name('products.edit');
+Route::put('/products/{product}/update', [ProductController::class, 'update'])->name('products.update');
+Route::delete('/products/{product}/delete', [ProductController::class, 'destroy'])->name('products.destroy');
 
 
